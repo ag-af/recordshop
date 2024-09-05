4e40bb9ab35b451a7f9df8ca3e7c3bbef0b65440
ag-af <agabialves11@gmail.com>
2024-09-05 10:09:34 +0100

    Initial commit: Record Shop API Project

---
+++ src/main/java/com/northcoders/recordshop/RecordShopApiMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopApiMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopApiMiniProjectApplication.class, args);
+	}
+
+}
